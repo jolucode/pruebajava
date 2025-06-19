df11f869a488a5c8e1f61403efc62c0b0d586d38
jolucode <157257214+jolucode@users.noreply.github.com>
2025-06-19 12:02:07 -0500

    chore: 19/06/2025 first commit

---
+++ src/main/java/com/java/pruebajava/PruebajavaApplication.java
@@ -0,0 +1,14 @@
+package com.java.pruebajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebajavaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		//SpringApplication.run(PruebajavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/pruebajava/PruebajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.pruebajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
